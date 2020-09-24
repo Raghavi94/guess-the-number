51568959777be9d98045d2d401bc555a176dccbc
Raghavi Srinivasan <53093082+Raghavi94@users.noreply.github.com>
2020-09-24 20:25:07 +0530

    Add files via upload

---
+++ list_to_dict_and_dict_to_list.py
@@ -0,0 +1,9 @@
+#list to dictionary
+list_alphabet=[(1,'a'),(2,'b'),(3,'c'),(4,'d')]
+dict_alphabet=dict(list_alphabet)
+print(dict_alphabet)
+
+#dictionary to list
+dict_alphabet={1:'a',2:'b',3:'c',4:'d'}
+list_alphabet=list(dict_alphabet.items())
+print(list_alphabet)
